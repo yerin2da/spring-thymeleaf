9c928a486965994f3ab7eb58c7cd5a19ad0f9bf0
yerin <byl06@naver.com>
2025-11-11 15:35:19 +0900

    basic commit

---
+++ src/test/java/com/yerin/spring_thymeleaf/SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yerin.spring_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
